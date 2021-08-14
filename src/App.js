cb13d45f1e4a03e385400cbd7607acbdd306f2b2
Charly <alctagne@gmail.com>
2021-08-14 14:09:06 -0400

    APP js and css added

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.CSS';
+import Header from './components/Header';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Index from './components/pages/Index';
+import Work from './components/pages/Work';
+import Contact from './components/pages/Contact';
+import About from './components/pages/About';
+
+
+function App() {
+  return (
+    <>
+    <Router basename='/reactportfolio'>
+      <Header />
+      <Switch>
+        <Route path='/' exact component={Index}/>
+        <Route path='/Work' exact component={Work}/>
+        <Route path='/Contact' exact component={Contact}/>
+        <Route path='/About' exact component={About}/>
+      </Switch>
+    </Router>
+    
+  </>
+  );
+}
+
+export default App;
